498b69aeae80280cffbfe070e4eec61f1d937b66
Alex Sifuna <aleksifuna@gmail.com>
2024-04-11 01:31:48 +0300

    Added a function that runs a function passed in the argument and returns a list of outcome

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -2,6 +2,6 @@ export default function guardrail(mathFunction) {
   try {
     return [mathFunction(), 'Guardrail was processed'];
   } catch (e) {
-    return [e.message, 'Guardrail was processed'];
+    return [String(e), 'Guardrail was processed'];
   }
 }
