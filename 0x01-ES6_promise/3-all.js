1e8580df4be61b1be793d2268f39fd333085e6f4
Alex Sifuna <aleksifuna@gmail.com>
2024-04-11 00:01:23 +0300

    added a function that resolves all promises and logs to the console

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -5,7 +5,7 @@ export default function handleProfileSignup() {
   const prom2 = createUser();
   const promAll = Promise.all([prom1, prom2]);
   return promAll.then((results) => {
-    console.log(results[1].firstName, results[1].lastName);
+    console.log(results[0].body, results[1].firstName, results[1].lastName);
   })
     .catch(() => console.log('Signup system offline'));
 }
