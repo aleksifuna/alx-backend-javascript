bd64bcbc1316a56b7e1227da0de0198ddec40643
Alex Sifuna <aleksifuna@gmail.com>
2024-04-10 23:19:22 +0300

    added a fnction that waits on a promise and return objects based on the outcome

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,6 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => ({
-    status: 200,
-    body: 'success',
-  }), () => new Error()).then(() => console.log('Got a response from the API'));
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .then(() => console.log('Got a response from the API'));
 }
