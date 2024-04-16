d6eed161305278cdb091487181479b60dc24dc75
Alex Sifuna <aleksifuna@gmail.com>
2024-04-17 00:40:53 +0300

    added a function that returns a string of all the set values that start with a specific string

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
 export default function cleanSet(set, startString) {
   // returns a string of all set values that start with specific startString
   const result = [];
-  if (startString === '') {
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
     return '';
   }
 
