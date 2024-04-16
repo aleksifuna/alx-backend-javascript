91859849b91ea10eeec650f90ede60ddf167d47c
Alex Sifuna <aleksifuna@gmail.com>
2024-04-16 21:20:55 +0300

    added a function that returns a list of objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,9 @@
+export default function getListStudents() {
+  // creates and returns a list of objects
+
+  const guillaume = { id: 1, firstName: 'Guallaume', location: 'San Francisco' };
+  const james = { id: 2, firstName: 'James', location: 'Columbia' };
+  const serena = { id: 5, firstName: 'Serena', location: 'San Francisco' };
+
+  return [guillaume, james, serena];
+}
---
+++ 0x03-ES6_data_manipulation/README.md
@@ -0,0 +1,5 @@
+# 0x03. ES6 data manipulation
+## Learning Objectives
+1. How to use map, filter and reduce on arrays
+2. Typed arrays
+3. The Set, Map, and Weak link data structures
