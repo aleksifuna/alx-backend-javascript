e00bb1add6c8a5bf71e0508c64a7452225e0c515
Alex Sifuna <aleksifuna@gmail.com>
2024-04-18 05:16:43 +0300

    added some functions on the main file

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -39,3 +39,14 @@ export function createEmployee (salary: string | number): Director | Teacher {
     }
     return new Director
 }
+
+export function isDirector (employee: Director | Teacher) {
+    return employee instanceof Director;
+}
+
+export function executeWork (employee: Director | Teacher) {
+    if (isDirector(employee)) {
+        return (employee as Director).workDirectorTasks();
+    }
+    return (employee as Teacher).workTeacherTasks();
+}
