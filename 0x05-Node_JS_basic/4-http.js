7ae0b3bdb90b0f00601f0266a44eff0a349fa48f
Alex Sifuna <aleksifuna@gmail.com>
2024-06-05 02:48:12 +0300

    added a program that creates a small http server using http module

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,12 @@
+/**
+ * small HTTP server using http module
+ */
+
+const { createServer } = require('http');
+
+const app = createServer((request, response) => {
+  response.statusCode = 200;
+  response.setHeader('Content-Type', 'text/plain');
+  response.end('Hello Holberton School!');
+});
+app.listen(1245);
