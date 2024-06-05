0f0cb3d3aefd953e3b4c30872db3f239eeb1e31f
Alex Sifuna <aleksifuna@gmail.com>
2024-06-05 04:30:12 +0300

    added a more complex http server using express

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const fs = require('fs');
+
+const DBName = process.argv.length > 2 ? process.argv[2] : '';
+
+function countStudents(file) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, 'utf-8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+      }
+      if (data) {
+        const fileLines = data
+          .trim()
+          .split('\n');
+        const responseReport = [];
+        const fields = {};
+        const csvHeader = fileLines[0].split(',');
+        const studentHeader = csvHeader.slice(0, csvHeader.length - 1);
+        for (const line of fileLines.slice(1)) {
+          const studentRecord = line.split(',');
+          const studentPropeties = studentRecord
+            .slice(0, studentRecord.length - 1);
+          const field = studentRecord[studentRecord.length - 1];
+          if (!Object.keys(fields).includes(field)) {
+            fields[field] = [];
+          }
+          const studentEntry = studentHeader
+            .map((fieldName, idx) => [fieldName, studentPropeties[idx]]);
+          fields[field].push(Object.fromEntries(studentEntry));
+        }
+        const totalStudents = Object
+          .values(fields)
+          .reduce((pre, cur) => (pre || []).length + cur.length);
+        responseReport.push(`Number of students: ${totalStudents}`);
+        for (const [field, group] of Object.entries(fields)) {
+          responseReport.push([
+            `Number of students in ${field}: ${group.length}.`,
+            'List:',
+            group.map((student) => student.firstname).join(', '),
+          ].join(' '));
+        }
+        resolve(responseReport.join('\n'));
+      }
+    });
+  });
+}
+
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', (req, res) => {
+  const responseParts = ['This is the list of our students'];
+  countStudents(DBName)
+    .then((report) => {
+      responseParts.push(report);
+      res.end(responseParts.join('\n'));
+    })
+    .catch((err) => {
+      responseParts.push(err instanceof Error ? err.message : err.toString());
+      res.end(responseParts.join('\n'));
+    });
+});
+app.listen(1245);
+module.exports = app;
