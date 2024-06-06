0064e437e9941e9d2c90a032b67616c4b15ef045
Alex Sifuna <aleksifuna@gmail.com>
2024-06-07 00:50:29 +0300

    made some changes to the test

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,12 +2,16 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
 describe('calculate number', () => {
-  it('should return sum', () => {
+  it('test with whole numbers', () => {
     const sum1 = calculateNumber(1, 3);
+    assert.strictEqual(sum1, 4);
+  });
+  it('test with one  float and whole number', () => {
     const sum2 = calculateNumber(1, 3.7);
-    const sum3 = calculateNumber(1.2, 3.7);
-    assert.equal(sum1, 4);
     assert.strictEqual(sum2, 5);
+  });
+  it('test with both argments float', () => {
+    const sum3 = calculateNumber(1.2, 3.7);
     assert.strictEqual(sum3, 5);
   });
 });
