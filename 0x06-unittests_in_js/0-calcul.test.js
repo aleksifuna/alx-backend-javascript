8c60be5a657b06c11313d60d282a981041b96105
Alex Sifuna <aleksifuna@gmail.com>
2024-06-07 00:24:22 +0300

    added a test for calc

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,13 +1,13 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
-describe('Calculate number', function() {
-    it('should return sum', function () {
-        const sum1 = calculateNumber(1, 3);
-        const sum2 = calculateNumber(1, 3.7);
-        const sum3 = calculateNumber(1.2, 3.7);
-        assert(sum1 === 4);
-        assert(sum2 === 5);
-        assert(sum3 === 5);
-    });
+describe('calculate number', () => {
+  it('should return sum', () => {
+    const sum1 = calculateNumber(1, 3);
+    const sum2 = calculateNumber(1, 3.7);
+    const sum3 = calculateNumber(1.2, 3.7);
+    assert.equal(sum1, 4);
+    assert.strictEqual(sum2, 5);
+    assert.strictEqual(sum3, 5);
+  });
 });
