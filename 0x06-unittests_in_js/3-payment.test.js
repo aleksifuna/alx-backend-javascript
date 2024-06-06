ac82b1240a0bd9b8d275924d8657a5ddd92adf93
Alex Sifuna <aleksifuna@gmail.com>
2024-06-07 02:35:57 +0300

    added a function and test for spy

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -3,11 +3,12 @@ const sinon = require('sinon');
 const sendPaymentRequestToApi = require('./3-payment');
 const utils = require('./utils');
 
-describe.only('sendPaymentRequestToApi', () => {
+describe('sendPaymentRequestToApi', () => {
   it('utils.calculateNumber should be called accordingly', () => {
     const spy = sinon.spy(utils, 'calculateNumber');
     sendPaymentRequestToApi(100, 20);
     expect(spy.calledOnce).to.be.true;
     expect(spy.calledWith(100, 20)).to.be.true;
+    spy.restore();
   });
 });
